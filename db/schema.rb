c9f3354bf984150ef3ceb622245df7e698fca5f5
Vedansh Agrawal <agrawal.vedansh@gmail.com>
2024-12-21 16:25:55 +0530

    Add slug to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_13_024147) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_21_104110) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_13_024147) do
     t.boolean "is_bloggable", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
 end
