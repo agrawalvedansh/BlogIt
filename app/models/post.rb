f5bac16e3da8df0ed7d22e8cf8baf9e9569c986a
Vedansh Agrawal <agrawal.vedansh@gmail.com>
2024-12-28 14:18:42 +0530

    Add association between posts and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   MAX_DESCRIPTION_LENGTH = 10000
+
+  has_and_belongs_to_many :categories
+
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
   validates_inclusion_of :is_bloggable, in: [true, false]
